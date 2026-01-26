dbcd1bbaed174c614d6af62c9c34b0af8a1289ba
Dáryl <74144016+Halfcors@users.noreply.github.com>
2026-01-26 00:44:49 +0000

    Add files via upload
    Versión 4: cambios en interfaz y validaciones

---
+++ src/main/java/com/uisrael/ProyectoImagenEstutio/ProyectoImagenEstutioApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.ProyectoImagenEstutio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoImagenEstutioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoImagenEstutioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uisrael/ProyectoImagenEstutio/ProyectoImagenEstutioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.ProyectoImagenEstutio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoImagenEstutioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
